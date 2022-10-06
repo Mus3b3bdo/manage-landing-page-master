63be1f6252a67b4b8d5a30afffb02fe60ceaab04
Mus3b3bdo <musabashra1995@gmail.com>
2022-10-06 11:00:21 +0200

    Layout establishment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Grid from "@mui/material/Grid";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        <Grid item>Header</Grid>
+        <Grid item>banner</Grid>
+        <Grid item>content</Grid>
+        <Grid item>cprasol</Grid>
+        <Grid item>footer</Grid>
+      </Grid>
     </div>
   );
 }
